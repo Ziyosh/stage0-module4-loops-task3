2a180900dd2fa922fbbca43ab4a372296208ac8f
Ziyosh <ziyodaakhmedova1998@gmail.com>
2024-03-13 22:12:28 +0500

    last

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,18 +1,19 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t){
+    public void printDigitsSum(int t) {
 
-        int sum;
-        int k;
+        int sum = 0;
+        String numberString = String.valueOf(t);
+        int length = numberString.length();
 
-
-        for(sum=0; t!=0; t=t/10) {
-            if (t <= 0 || t >= 0) {
-                k = t % 10;
-                sum += k;
-            }
+            for (int i = 0; i < length; i++) {
+                int digit = numberString.charAt(i) - '0';
+                sum += digit;
         }
-            System.out.println(sum);
+
+        System.out.println(sum);
+
+
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -3,5 +3,13 @@ package school.mjc.stage0.loops.task3;
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
 
+        int sum=0, x=0;
+
+        for(int i=1; i<=lengthOfLastNumber; i++){
+            x=x*10 + 9;
+            sum = sum + x;
+        }
+        System.out.println(sum);
+
     }
 }
